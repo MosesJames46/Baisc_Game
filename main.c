0444797bb610a77ea164d37ce85344d6cab5a595
Momo <mosesjames39212@gmail.com>
2026-03-12 16:28:17 -0500

    Adjusting Console Control to use array instead of console for control

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include "header_files/console_control.h"
 #include "header_files/entity.h"
 #include "header_files/game_math.h"
+#include <stdio.h>
+
 
 int main(void){
+    printf("Hi my name is moses. Hopefully you're having a great day\n");
+
+    printf("\n I am thank you\n");
+    //printf("Printing Cursor position: \e[6n \n");
+    printf("\e[6n\n");
 
     return 0;
 }
