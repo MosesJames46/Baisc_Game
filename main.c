8eeea889dfee378e3244de43641c98b7e5bfa1ce
Momo <mosesjames39212@gmail.com>
2026-03-13 08:48:15 -0500

    Solved why malloc was not working

--- main.c
+++ main.c
@@ -1,15 +1,23 @@
 #include "header_files/console_control.h"
-#include "header_files/entity.h"
-#include "header_files/game_math.h"
+#include "header_files/game_utility.h"
 #include <stdio.h>
 
-
 int main(void){
-    printf("Hi my name is moses. Hopefully you're having a great day\n");
-
-    printf("\n I am thank you\n");
-    //printf("Printing Cursor position: \e[6n \n");
-    printf("\e[6n\n");
+    struct console_struct console;
+    console.array = NULL;
+    console.width = 256;
+    console.height = 256;
+    if(!console.array){
+        printf("Array is NULL.\n");
+    }
+    //console.array = malloc(sizeof(int) * console.width * console.height);
+   console.array = allocate_2Darray(console.width, console.height, console.array);
+    if(console.array){
+        printf("Array is not NULL.\n");
+    }
+    //printf("The status of malloc: %d\n", successful_malloc);
+    //int successful_free = deallocate_2Darray(console.array);
+    //printf("The status of deallocation: %d", successful_malloc);
 
     return 0;
 }
