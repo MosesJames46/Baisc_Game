3e4ec20ed4aa2b3bb1699d82e804955228d1df28
Momo <mosesjames39212@gmail.com>
2026-03-13 12:46:21 -0500

    Implemented proper array initialization

--- main.c
+++ main.c
@@ -6,13 +6,10 @@ int main(void){
     struct console_struct console;
     console.width = 256;
     console.height = 256;
-    //allocate_2Darray(console.width, console.height, &console.screen);
-    //init_screen(console.width, console.height, console.screen);
-    //print_to_screen(console.width, console.height, console.screen);
-    //int success = deallocate_2Darray(console.width, console.height, console.screen);
+    allocate_2Darray(console.width, console.height, &console.screen);
+    init_screen(console.width, console.height, &console.screen);
+    print_to_screen(console.width, console.height, &console.screen);
+    int success = deallocate_2Darray(console.width, console.height, console.screen);
 
-    int* some_array;
-    allocate_1Darray(10, &some_array);
-    deallocate_1Darray(some_array);
     return 0;
 }
