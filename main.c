49eb7807018332e1d90b70a9b8818547e03135b2
Momo <mosesjames39212@gmail.com>
2026-03-13 12:40:42 -0500

    Understanding of Malloc and Free is a bit better

--- main.c
+++ main.c
@@ -4,20 +4,15 @@
 
 int main(void){
     struct console_struct console;
-    console.array = NULL;
     console.width = 256;
     console.height = 256;
-    if(!console.array){
-        printf("Array is NULL.\n");
-    }
-    //console.array = malloc(sizeof(int) * console.width * console.height);
-   console.array = allocate_2Darray(console.width, console.height, console.array);
-    if(console.array){
-        printf("Array is not NULL.\n");
-    }
-    //printf("The status of malloc: %d\n", successful_malloc);
-    //int successful_free = deallocate_2Darray(console.array);
-    //printf("The status of deallocation: %d", successful_malloc);
+    //allocate_2Darray(console.width, console.height, &console.screen);
+    //init_screen(console.width, console.height, console.screen);
+    //print_to_screen(console.width, console.height, console.screen);
+    //int success = deallocate_2Darray(console.width, console.height, console.screen);
 
+    int* some_array;
+    allocate_1Darray(10, &some_array);
+    deallocate_1Darray(some_array);
     return 0;
 }
